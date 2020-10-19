7055c676d9b90660ef16d2596c798ea704c19771
Kelin Field <kfield111@hotmail.com>
2020-10-19 23:40:24 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/name'  do
+    "My name is Kelin Field."
+  end
+
+  get '/hometown' do
+    "My hometown is Vienna, IL."
+  end
+
+  get '/favorite-song' do
+    "My favorite song is All Fall Down."
+  end
+
 end
